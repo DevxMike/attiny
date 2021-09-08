9317f2333ec7458f14d4ff4ca4cc7ef4d070c432
mike <michalbaz@poczta.onet.pl>
2021-09-08 21:44:16 +0200

    spi ready

---
+++ spi.c
@@ -0,0 +1,26 @@
+#include "spi.h"
+#include <util/delay.h>
+
+void init_spi(void){
+    DDR_CLK |= (1 << CLK);
+    DDR_MOSI |= (1 << MOSI);
+}
+
+void send_byte(uint8_t byte){
+    for(uint8_t i = 0; i < 8; ++i){
+        _delay_us(0.1);
+        PORT_CLK |= (1 << CLK);
+        
+        if(byte & (1 << i)){
+            if(!(PORT_MOSI & (1 << MOSI))){
+                PORT_MOSI |= (1 << MOSI);
+            }
+        }
+        else{
+            PORT_MOSI &= ~(1 << MOSI);
+        }
+
+        _delay_us(0.1);
+        PORT_CLK &= ~(1 << CLK);
+    }
+}
--- spi.h
+++ spi.h
@@ -1,9 +1,17 @@
 #ifndef spi_h_
 #define spi_h_
 
+#define MOSI 0
+#define PORT_MOSI PORTB
+#define DDR_MOSI DDRB
+
+#define CLK 1 
+#define PORT_CLK PORTB
+#define DDR_CLK DDRB
+
 #include <avr/io.h>
 
-void init_spi(uint16_t baud);
+void init_spi(void);
 void send_byte(uint8_t byte);
 
 #endif
