47237966fa6717d15ff828e5220a5c03a3f398e1
mike <michalbaz@poczta.onet.pl>
2021-09-08 21:55:26 +0200

    send set of data to shift registers

---
+++ led_disp.c
@@ -0,0 +1,9 @@
+#include "led_disp.h"
+#include "spi.h"
+
+void send_set(const led_disp_data_t* data){
+    send_byte(data->second);
+    send_byte(data->first);
+    PORT_LATCH |= (1 << LATCH); //wyłącz wyjścia rejestrów i przenieś zawartość rejestru przesuwnego do wyjść
+    PORT_LATCH &= ~(1 << LATCH); //włącz wyjścia
+}
